392ba10152e6f0b181161e3daa0478e6a4f432a4
Rodrigo Álvarez <incognia@gmail.com>
2019-09-15 10:09:34 -0500

    Create [parlamentarios]index.php

---
+++ [parlamentarios]index.php
@@ -0,0 +1,75 @@
+<?php
+	//phpinfo();
+
+	//$ruta = "oficialia/di/";
+
+	//if ($handle = opendir($ruta)) {
+	if ($handle = opendir('.')) {
+		while (false !== ($entry = readdir($handle))) {
+			//if ($entry != "." && $entry != "..") {
+			if ($entry != ".") {
+				//echo "<br>$entry";
+
+				$ext = pathinfo($entry, PATHINFO_EXTENSION);
+				$array_tipos=array(
+						"folder"=>"../img/folder.png"
+                                                ,"txt"=>"../img/text.png"
+                                                ,"pdf"=>"../img/pdf.png"
+                                                ,"xlsx"=>"../img/calc.png"
+                                                ,"xls"=>"../img/calc.png"
+                                                ,"csv"=>"../img/calc.png"
+                                                ,"rtf"=>"../img/doc.png"
+                                                ,"docx"=>"../img/doc.png"
+                                                ,"doc"=>"../img/doc.png"
+                                                ,""=>"../img/folder-home.png"
+				);
+				
+				//echo "extension: [".$ext."]<br>";
+				
+				switch($ext){
+					/*case "txt":
+						echo "<br><a href='".$ruta."$entry' target='_blank'><img src='".$array_tipos[$ext]."'> ".$entry."</a>";
+						break;*/
+					
+					case "":
+                                                //echo "<br><a href='".$ruta."$entry'><img src='".$array_tipos[$ext]."'> ".$entry."</a>";
+                                                echo "<br><a href='".$ruta."$entry'><img src='".$array_tipos[$ext]."'> Regresar...</a>";
+                                                break;
+					
+					case "pdf":
+                                                echo "<br><a href='".$ruta."$entry' target='_blank'><img src='".$array_tipos[$ext]."'> ".$entry."</a>";
+                                                break;
+					
+					case "xlsx":
+                                                echo "<br><a href='".$ruta."$entry' target='_blank'><img src='".$array_tipos[$ext]."'> ".$entry."</a>";
+                                                break;
+
+					case "xls":
+                                                echo "<br><a href='".$ruta."$entry' target='_blank'><img src='".$array_tipos[$ext]."'> ".$entry."</a>";
+                                                break;
+					
+					case "csv":
+                                                echo "<br><a href='".$ruta."$entry' target='_blank'><img src='".$array_tipos[$ext]."'> ".$entry."</a>";
+                                                break;
+					
+					case "rtf":
+                                                echo "<br><a href='".$ruta."$entry' target='_blank'><img src='".$array_tipos[$ext]."'> ".$entry."</a>";
+                                                break;
+
+					case "docx":
+                                                echo "<br><a href='".$ruta."$entry' target='_blank'><img src='".$array_tipos[$ext]."'> ".$entry."</a>";
+                                                break;
+
+					case "doc":
+                                                echo "<br><a href='".$ruta."$entry' target='_blank'><img src='".$array_tipos[$ext]."'> ".$entry."</a>";
+                                                break;
+					
+				};
+				
+			}
+		}
+
+		closedir($handle);
+
+	}
+?>
